908564434303a21f7577ade9110cf5d60f1e80f0
tana-gh <yozoranotakaku@gmail.com>
2021-06-19 09:44:15 +0900

    Remove jquery from test

--- spec/components/ace.spec.ts
+++ spec/components/ace.spec.ts
@@ -1,18 +1,17 @@
-import $        from 'jquery'
 import * as Ace from '../../src/components/ace'
 
 describe('appendAce', () => {
     it('should be appended correctly', async () => {
-        const editor = $('<div></div>')
-        const body   = $('body').append(editor)
+        const editor = document.createElement('div')
+        const body   = document.body
 
-        expect(editor.children()).toHaveLength(0)
-        expect(body.children('script[src*="/ace.min.js"]')).toHaveLength(0)
+        expect(editor.children).toHaveLength(0)
+        expect(body.querySelectorAll('script[src*="/ace.min.js"]')).toHaveLength(0)
 
         const appendAceSequence = async () => {
-            const promise = Ace.bindAce(editor.get(0))
-            const script  = body.children('script[src*="/ace.min.js"]').get(0)
-            script.dispatchEvent(new Event('load'))
+            const promise = Ace.bindAce(editor)
+            const script  = body.querySelector('script[src*="/ace.min.js"]')
+            script?.dispatchEvent(new Event('load'))
             await promise
         }
 
